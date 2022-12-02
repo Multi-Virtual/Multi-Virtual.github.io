e20f6b4d716c35b11a7e08f597ad3a636c0749cb
Multi-Virtual <111546945+Multi-Virtual@users.noreply.github.com>
2022-12-01 22:30:27 -0500

    Update snowstorm.js

--- snowstorm.js
+++ snowstorm.js
@@ -1,9 +1,13 @@
 // 1. Define a color for the snow
 snowStorm.snowColor = '#fff';
+
 // 2. To optimize, define the max number of flakes that can
 // be shown on screen at once
-snowStorm.flakesMaxActive = 100;
+snowStorm.flakesMaxActive = 35; // Coolboy here, I set the number to 35 so it's not as intense on 
+                                // less powerful hardware.
+
 // 3. Allow the snow to flicker in and out of the view
 snowStorm.useTwinkleEffect = true; 
+
 // 4. Start the snowstorm!
 snowStorm.start();
